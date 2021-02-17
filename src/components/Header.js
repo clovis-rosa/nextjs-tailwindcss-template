6fbbf7bf8a4f2ece3c11640eb469016b3e0a627d
clovis-rosa <clovis.souzarosa@gmail.com>
2021-02-17 02:16:42 +0000

    Code improvements on Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ export default function Header() {
 
   return (
     <>
-      <header className="relative" x-data="{ menuOpen: false }">
+      <header className="relative">
         <div className="pt-6 bg-white dark:bg-gray-900  transition duration-400">
           <nav
             className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
@@ -26,13 +26,13 @@ export default function Header() {
                   <a>
                     <span className="sr-only">Workflow</span>
                     <Image
+                      className="h-8 w-auto sm:h-10"
                       width={30}
                       height={30}
                       src={
                         "/assets/images/logos/workflow-mark-teal-200-cyan-400.svg"
                       }
-                      alt="some image description here"
-                      className="h-8 w-auto sm:h-10"
+                      alt="Branding logo"
                     />
                   </a>
                 </Link>
@@ -41,9 +41,10 @@ export default function Header() {
                   <button
                     onClick={handleClick}
                     type="button"
-                    className="bg-gray-200 dark:dg-gray-800 rounded-md p-2 inline-flex items-center justify-center text-gray-800 dark:text-gray-200 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white"
+                    className="bg-gray-200 dark:bg-gray-500 rounded-md p-2 inline-flex items-center justify-center text-gray-800 dark:text-gray-200 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white"
                     id="main-menu"
                     aria-haspopup="true"
+                    aria-label="Open main menu"
                   >
                     <span className="sr-only">Open main menu</span>
                     {/* <!-- Heroicon name: outline/menu --> */}
@@ -66,13 +67,8 @@ export default function Header() {
               </div>
               <div className="hidden space-x-8 md:flex md:ml-10">
                 {menuData.map((link) => (
-                  <Link
-                    key={link.title}
-                    href={link.href}
-                    // onClick={onToggleNav}
-                  >
-                    {/* <a className="text-base font-medium text-white hover:text-gray-300"> */}
-                    <a className="p-1 sm:p-4 text-gray-900 dark:text-gray-100 hover:text-gray-800">
+                  <Link key={link.title} href={link.href}>
+                    <a className="text-base font-medium text-gray-900 dark:text-gray-100 hover:bg-gray-100 tracking-normal px-4 py-2 rounded">
                       {link.title}
                     </a>
                   </Link>
@@ -130,7 +126,7 @@ export default function Header() {
                       src={
                         "/assets/images/logos/workflow-mark-teal-500-cyan-600.svg"
                       }
-                      alt="some image description here"
+                      alt="Branding logo"
                     />
                   </a>
                 </Link>
@@ -139,8 +135,9 @@ export default function Header() {
               <div className="-mr-2">
                 <button
                   onClick={handleClick}
-                  type="button"
                   className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600"
+                  aria-label="Close menu"
+                  type="button"
                 >
                   <span className="sr-only">Close menu</span>
                   {/* <!-- Heroicon name: outline/x --> */}
@@ -169,11 +166,7 @@ export default function Header() {
             >
               <div className="px-2 space-y-1" role="none">
                 {menuData.map((link) => (
-                  <Link
-                    key={link.title}
-                    href={link.href}
-                    // onClick={onToggleNav}
-                  >
+                  <Link key={link.title} href={link.href}>
                     <a
                       className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
                       role="menuitem"
@@ -192,7 +185,7 @@ export default function Header() {
               </div>
               <div role="none" className="mt-6 px-5">
                 <p className="text-center text-base font-medium text-gray-500">
-                  Existing customer?{" "}
+                  Existing customer?
                   <Link href="/">
                     <a className="text-gray-900 hover:underline">Login</a>
                   </Link>
