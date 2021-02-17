bab16943a3e66d34a623e48949cad9955fb012b7
clovis-rosa <clovis.souzarosa@gmail.com>
2021-02-17 01:29:09 +0000

    Implemented theme switcher component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { menuData } from "../data/menuData";
-import { useTheme } from "next-themes";
+import BtnThemeSwitch from "./BtnThemeSwitch";
+// import { useTheme } from "next-themes";
 
 export default function Header() {
-  const { theme, setTheme } = useTheme();
+  // const { theme, setTheme } = useTheme();
 
   const [click, setClick] = useState(false);
 
@@ -80,7 +81,7 @@ export default function Header() {
             </div>
             <div className="hidden md:flex md:items-center md:space-x-6">
               <Link href="/">
-                <a className="text-base font-medium text-gray-900  hover:text-gray-800">
+                <a className="text-base font-medium text-gray-900 dark:text-gray-100  hover:text-gray-800">
                   Log in
                 </a>
               </Link>
@@ -89,24 +90,12 @@ export default function Header() {
                   Start free trial
                 </a>
               </Link>
-              <button
-                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-                id="switchTheme"
-                className="h-10 w-10 flex justify-center items-center focus:outline-none text-yellow-500"
-              >
-                <svg
-                  className="w-6 h-6"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </button>
+              {/* <!--
+              //
+            Button theme switch
+            //
+          --> */}
+              <BtnThemeSwitch />
             </div>
           </nav>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -22,19 +22,10 @@ export default function Home() {
           variants={fadeInUp}
           className="text-center max-w-screen-lg xl:max-w-screen-xl mx-auto"
         >
-          <h1 className="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight text-gray-900 mt-10 mb-8 sm:mt-14 sm:mb-10">
+          <h1 className="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight text-gray-900 dark:text-white mt-10 mb-8 sm:mt-14 sm:mb-10">
             Rapidly build modern websites without ever leaving your HTML.
           </h1>
-          <Link href="/">
-            <motion.a
-              initial={{ y: 60, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              transition={{ delay: 0.2 }}
-              className="w-full sm:w-auto flex-none bg-gray-900 hover:bg-gray-700 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200"
-            >
-              Get started
-            </motion.a>
-          </Link>
+
           <motion.p
             initial={{ y: 60, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
